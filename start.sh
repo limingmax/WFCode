f2e6d2fcf05ad9fdaac3a69603afee047ed37ca3
limingmax <limingmax@163.com>
2018-12-07 16:54:05 +0800

    test

--- start.sh
+++ start.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-sed -i "s/HBASE_IP/= ${HBASE_IP}/g" python.ini
-sed -i "s/HBASE_PORT/= ${HBASE_PORT}/g" python.ini
+sed -i "s/HBASE_IP/${HBASE_IP}/g" python.ini
+sed -i "s/HBASE_PORT/${HBASE_PORT}/g" python.ini
 
 sed -i "s/REDIS_IP:REDIS_PORT/${REDIS_IP}:${REDIS_PORT}/g" python.ini
 
